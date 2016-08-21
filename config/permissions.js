f1eb10a0f04dc47f34266b293fef940ebb386197
jaumard <jimmy.aumard@gmail.com>
2016-08-21 20:43:58 +0200

    update acl trailpack

--- config/permissions.js
+++ config/permissions.js
@@ -3,6 +3,7 @@
 module.exports = {
   defaultRole: 'public', //Role name to use for anonymous users
   userRoleFieldName: 'roles', // Name of the association field for Role under User model
+  modelsAsResources: true,
   //Initial data added when DB is empty
   fixtures: {
     roles: [{
