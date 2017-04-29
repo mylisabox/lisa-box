d06a1d7902922b645693e3b0e26b2e6134284caa
Jimmy Aumard <Jimmy.Aumard@nestle.com>
2017-04-29 11:27:17 +0200

    remove serial, bonjour, hotword on test env

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -20,36 +20,38 @@ module.exports = (app) => {
   })
   service.start()
 
-  app.serialPort = serialPort
-  app.bonjour = bonjour
   app.lisa = new LISA(app)
+  if (app.env.NODE_ENV != 'testing') {
 
-  const Sonus = require('sonus')
-  const speech = require('@google-cloud/speech')({
-    keyFilename: './config/speech/LISA-gfile.json'
-  })
+    app.serialPort = serialPort
+    app.bonjour = bonjour
 
-  const hotwords = [{file: './config/speech/hey_lisa.pmdl', hotword: 'hey lisa'}]
-  const language = 'fr-FR'
-  const sonus = Sonus.init({hotwords, language}, speech)
-  Sonus.start(sonus)
-  sonus.on('hotword', (index, keyword) => app.log.debug('hey lisa detected'))
-  sonus.on('final-result', sentence => {
-    app.log.debug(sentence + ' detected')
-    app.services.ChatBotService.interact(null, language.substring(0, 2) || this.app.config.chatbot.defaultLang,
-      sentence, null)
-      .then(result => {
-        app.log.debug('bot results')
-        app.log.debug(JSON.stringify(result))
-        return app.services.PluginService.interact(result).then(results => {
-          app.log.debug('plugin results')
-          app.log.debug(results)
-        })
-      }).catch(err => {
-      app.log.error(err)
+    const Sonus = require('sonus')
+    const speech = require('@google-cloud/speech')({
+      keyFilename: './config/speech/LISA-gfile.json'
     })
-  })
 
+    const hotwords = [{file: './config/speech/hey_lisa.pmdl', hotword: 'hey lisa'}]
+    const language = 'fr-FR'
+    const sonus = Sonus.init({hotwords, language}, speech)
+    Sonus.start(sonus)
+    sonus.on('hotword', (index, keyword) => app.log.debug('hey lisa detected'))
+    sonus.on('final-result', sentence => {
+      app.log.debug(sentence + ' detected')
+      app.services.ChatBotService.interact(null, language.substring(0, 2) || this.app.config.chatbot.defaultLang,
+        sentence, null)
+        .then(result => {
+          app.log.debug('bot results')
+          app.log.debug(JSON.stringify(result))
+          return app.services.PluginService.interact(result).then(results => {
+            app.log.debug('plugin results')
+            app.log.debug(results)
+          })
+        }).catch(err => {
+        app.log.error(err)
+      })
+    })
+  }
   /*
    app.services.PluginService._addPlugin('lisa-plugin-hue').then(plugin => {
    console.log(plugin, app)
