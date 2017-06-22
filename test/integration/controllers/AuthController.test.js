722123a00b6326ff15dd687e9674fdb1a5b6c568
Jimmy Aumard <Jimmy.Aumard@nestle.com>
2017-06-22 21:03:37 +0200

    Fix tests

--- test/integration/controllers/AuthController.test.js
+++ test/integration/controllers/AuthController.test.js
@@ -6,77 +6,63 @@ const supertest = require('supertest')
 
 describe('AuthController', () => {
   let request, token, prefix
-  before((done) => {
+  before(() => {
     request = supertest('http://localhost:3000')
     prefix = global.app.config.footprints.prefix
-    request
-      .post(prefix + '/auth/local/register')
-      .set('Accept', 'application/json') //set header for this test
-      .send({username: 'jaumard', password: 'adminadmin', email: 'test@test.te'})
-      .expect(200)
-      .end((err, res) => {
-        assert.equal(res.body.redirect, '/')
-        assert.equal(res.body.user.id, 1)
-        token = res.body.token
-        done(err)
-      })
+
   })
   it('should exist', () => {
     assert(global.app.api.services['PassportService'])
     assert(global.app.services['PassportService'])
   })
 
-  it('should insert a user on /auth/local/register', (done) => {
-    request
-      .post(prefix + '/auth/local/register')
-      .set('Accept', 'application/json') //set header for this test
-      .send({username: 'jim', password: 'adminadmin'})
-      .expect(200)
-      .end((err, res) => {
-        assert.equal(res.body.redirect, '/')
-        assert.equal(res.body.user.id, 2)
-        assert.equal(res.body.user.username, 'jim')
-        done(err)
-      })
-  })
-
   it('should return an error on missing passport for registration on /auth/local/register', (done) => {
     request
       .post(prefix + '/auth/local/register')
       .set('Accept', 'application/json') //set header for this test
-      .send({username: 'yoyo'})
+      .send({ username: 'yoyo' })
       .expect(400)
       .end((err, res) => {
         done(err)
       })
   })
 
-  it('should insert a user on /auth/local/register with form submit', (done) => {
+  it('should insert a user on /auth/local/register', (done) => {
     request
       .post(prefix + '/auth/local/register')
-      .set('Content-Type', 'application/x-www-form-urlencoded')
-      .send('username=jim2&password=adminadmin2')
       .set('Accept', 'application/json') //set header for this test
+      .send({ password: 'adminadmin', email: 'test@test.te' })
       .expect(200)
       .end((err, res) => {
+        token = res.body.token
         assert.equal(res.body.redirect, '/')
-        assert.equal(res.body.user.id, 3)
-        assert.equal(res.body.user.username, 'jim2')
+        assert.equal(res.body.user.id, 1)
+        assert.equal(res.body.user.email, 'test@test.te')
         done(err)
       })
   })
 
+  it('should return 403 because a user already exist', (done) => {
+    request
+      .post(prefix + '/auth/local/register')
+      .set('Accept', 'application/json') //set header for this test
+      .send({ username: 'yoyo' })
+      .expect(403)
+      .end((err, res) => {
+        done(err)
+      })
+  })
 
   it('should log a user on /auth/local', (done) => {
     request
       .post(prefix + '/auth/local')
       .set('Accept', 'application/json') //set header for this test
-      .send({email: 'test@test.te', password: 'adminadmin'})
+      .send({ email: 'test@test.te', password: 'adminadmin' })
       .expect(200)
       .end((err, res) => {
         assert.equal(res.body.redirect, '/')
         assert.equal(res.body.user.id, 1)
-        assert.equal(res.body.user.username, 'jaumard')
+        assert.equal(res.body.user.email, 'test@test.te')
         assert(res.body.token)//JWT token
         done(err)
       })
---
+++ test/integration/policies/RegisterPolicy.test.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const assert = require('assert')
+
+describe('Register', () => {
+  it('should exist', () => {
+    assert(global.app.policies.RegisterPolicy)
+  })
+})
