d6afd29954cedcb33ebd2ba91f1c6b80b3aa1bf6
jaumard <Jimmy.Aumard@nestle.com>
2017-11-25 17:44:36 +0100

    update install script

--- scripts/installPi.sh
+++ scripts/installPi.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 apt-get update
-#apt-get upgrade -y
+apt-get upgrade -y
 
 apt-get install -y curl
 apt-get install -y build-essential
 
 #install node
 if which node > /dev/null ; then
-    curl -sL https://deb.nodesource.com/setup_7.x | bash -
+    curl -sL https://deb.nodesource.com/setup_8.x | bash -
     apt-get install -y nodejs
 else
     echo "node is installed, skipping..."
@@ -28,14 +28,16 @@ apt-get install -y git mongodb-server
 #sox install for sonus speech recognition
 apt-get install -y sox libsox-fmt-all alsa-utils libatlas-base-dev
 
+# others
+apt-get install -y libzmq3-dev libavahi-compat-libdnssd-dev
+
 #install lirc
 apt-get install -y lirc
 
 #matrix board
 echo "deb http://packages.matrix.one/matrix-creator/ ./" | sudo tee --append /etc/apt/sources.list
 apt-get update
-apt-get upgrade
-apt-get install -y libzmq3-dev wiringpi cmake matrix-creator-init matrix-creator-malos
+apt-get install -y matrix-creator-openocd matrix-creator-init matrix-creator-malos --allow-unauthenticated
 echo 'export AUDIODEV=mic_channel8' >>~/.bash_profile
 echo 'export LANG=en-US' >>~/.bash_profile
 source ~/.bash_profile
@@ -51,6 +53,13 @@ git clone https://github.com/mylisabox/lisa-box
 cd lisa-box
 
 yarn
+yarn global add forever
+
+cd /etc/init.d/
+wget https://raw.githubusercontent.com/mylisabox/lisa-box/master/scripts/lisa
+chmod 755 /etc/init.d/lisa
+update-rc.d lisa defaults
+
 
 RED='\033[0;31m'
 NC='\033[0m'
