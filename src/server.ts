82e06f830a897eb713c83413498c124f23fbff02
Scott B. Wyatt <scott-wyatt@users.noreply.github.com>
2020-08-10 12:39:33 -0500

    fix(): remove leftover

--- src/server.ts
+++ src/server.ts
@@ -13,10 +13,6 @@ const app = new FabrixApp(App)
 
 app.start().catch(app.stop)
 
-if (app.log) {
-  app.log.debug('Hello world, welcome to RiSE!')
-}
-
 process.on('uncaughtException', function (err) {
   console.log('Caught exception: ' + err)
   console.log(err.stack)
