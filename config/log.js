e503d3ba6b5adfa5eb489f40f125a921c6dc3829
jaumard <jimmy.aumard@gmail.com>
2020-08-29 09:39:04 +0200

    Fix logs

--- config/log.js
+++ config/log.js
@@ -6,6 +6,18 @@
  */
 const winston = require('winston')
 
+const defaultLogger = winston.createLogger({
+  level: 'debug',
+  exitOnError: true,
+  format: winston.format.combine(
+    winston.format.colorize({all: true}),
+    winston.format.cli(),
+  ),
+  transports: [
+    new winston.transports.Console()
+  ]
+});
+
 module.exports = {
 
   /**
@@ -14,17 +26,35 @@ module.exports = {
    *
    * Exposed on app.log
    */
-  logger: winston.createLogger({
-    level: 'debug',
-    exitOnError: true,
-    format: winston.format.combine(
-      winston.format.colorize({all: true}),
-      winston.format.cli(),
-    ),
-    transports: [
-      new winston.transports.Console()
-    ]
-  }),
+  logger: {
+    silly: function () {
+      defaultLogger.silly([].slice.call(arguments).join(' '))
+    },
+    error: function () {
+      defaultLogger.error([].slice.call(arguments).join(' '))
+    },
+    debug: function () {
+      defaultLogger.debug([].slice.call(arguments).join(' '))
+    },
+    info: function () {
+      defaultLogger.info([].slice.call(arguments).join(' '))
+    },
+    warn: function () {
+      defaultLogger.warn([].slice.call(arguments).join(' '))
+    },
+    alert: function () {
+      defaultLogger.alert([].slice.call(arguments).join(' '))
+    },
+    crit: function () {
+      defaultLogger.crit([].slice.call(arguments).join(' '))
+    },
+    verbose: function () {
+      defaultLogger.verbose([].slice.call(arguments).join(' '))
+    },
+    warning: function () {
+      defaultLogger.warning([].slice.call(arguments).join(' '))
+    }
+  },
 
   pluginLogger: winston.createLogger({
     level: 'debug',
